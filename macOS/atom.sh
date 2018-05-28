4346ee1ba7eecd6accb04a9562003152d27fe4bd
y7usuf <me@yusufgoksu.com>
2018-05-28 14:10:02 +0300

    change the path

---
+++ macOS/atom.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+
+# Awesome Atom Packages
+# Before you run this shell script, make sure you have installed Atom
+
+apm install \
+atom-beautify \
+prettier-atom \
+file-icons \
+highlight-selected \
+atom-ternjs \
+atom-wrap-in-tag \
+autocomplete-modules \
+docblockr \
+language-babel \
+linter-eslint \
